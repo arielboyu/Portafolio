65754725a4f281df50c6e0c186fefa3e8d7d2f94
arielboyu <arielboyu@gmail.com>
2020-11-27 17:09:46 -0300

    primer commit

---
+++ src/components/About/about.js
@@ -0,0 +1,62 @@
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
+import './about.css'
+
+export default function About(){
+  const [info, setInfo] = useState({});
+  useEffect(() => {
+      axios.get('https://arielsalcedoportfolio.herokuapp.com/about')
+      .then(
+          res => setInfo(res.data[0])
+      )}, [setInfo])
+  return (
+    <div>
+    <div className="header">
+    <ion-icon style={{fontSize:"100px"}} name="person-outline"></ion-icon>
+      <h1>{info.name}</h1>
+      <h1>{info.profession}  </h1>
+      <ion-icon style={{fontSize:"100px"}} name="code-slash-outline"></ion-icon>
+    </div>
+    <div className="div-foto">
+        <img className="foto-perfil"  src={info.photo}></img>
+    </div>
+    <div className="About">
+        <div className="Descripcion">
+            <h2>Herramientas de Trabajo</h2>
+            <ion-icon style={{fontSize:"35px"}} name="logo-css3"></ion-icon>
+            <ion-icon style={{fontSize:"35px"}} name="logo-javascript"></ion-icon>
+            <ion-icon style={{fontSize:"35px"}}  name="logo-npm"></ion-icon>
+            <ion-icon style={{fontSize:"35px"}}  name="logo-react"></ion-icon>
+            <ion-icon style={{fontSize:"35px"}} name="logo-html5"></ion-icon>
+            <ion-icon style={{fontSize:"35px"}} name="logo-nodejs"></ion-icon>
+            <ion-icon style={{fontSize:"35px"}} name="logo-github"></ion-icon>
+            <p>{info.about_me}</p>
+            <img className="gif"  src={"https://static.wixstatic.com/media/669128_ec1c7a78e9694aec8a07c2e48b292ae1~mv2.gif"}></img>
+            <ul className="skills">
+                { info.skills  && info.skills.map((s) =>
+                    <p className="skill">{s} </p>
+                )}
+            </ul>
+        </div>
+     </div>
+     <div className="enlaces" >
+       <a href="https://github.com/arielboyu" target="_parent"><button type="button" class="btn btn-success">Github <ion-icon name="logo-github"></ion-icon></button></a>
+       <div class="col-sm-1 col-xs-1 col-md-1 col-lg-1"></div>
+      <a href="https://www.linkedin.com/in/ariel-salcedo-b423b61ba/" target="_parent"><button type="button" class="btn btn-primary">Linkedin  <ion-icon name="logo-linkedin"></ion-icon></button></a>
+     </div>
+     <div className="cardInfo" >
+     <p> <ion-icon name="accessibility-outline"></ion-icon> Bienvenidos a mi portfolio de presentación,mi nombre es Ariel , soy aficionado a la tecnología ,
+         con muchas ganas de aceptar desafios y aprender
+              siempre nuevas herramientas de trabajo   <ion-icon name="rocket-outline"></ion-icon>  </p>
+     </div>
+     <footer className="footer">
+    <div>
+    Contacto :
+    <a href="mailto:correo@electronico\_enlazado.com"> edgararielsalcedo@gmail.com </a>
+
+     <ion-icon name="chatbox-outline"></ion-icon>
+    </div>
+    </footer>
+    </div>
+  )
+}
---
+++ src/components/Portfolio/portfolio.js
@@ -0,0 +1,26 @@
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
+import './portfolio.css'
+
+export default function Portfolio(){
+  const [port, setPort] = useState({});
+  useEffect(() => {
+      axios.get('https://arielsalcedoportfolio.herokuapp.com/portfolio')
+      .then(
+          res => setPort(res.data)
+      )}, [setPort])
+  return (
+    <div >
+    <div className="mega-card-portfolio">
+              { port.length > 0 && port.map((p) =>
+                  <div className="individual-card">
+                      <h1>{p.name}</h1>
+                      <img className="imagen-proyecto" alt="imagen-proyecto" src={p.image}></img>
+                      <p>{p.descripcion}</p>
+                      <a className="boton-visitar" href={p.link}>Ver Proyecto</a>
+                  </div>
+              )}
+          </div>
+    </div>
+  )
+}
