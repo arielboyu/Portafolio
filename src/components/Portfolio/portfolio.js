5d10fd783b660d47a906ca54dfb8777bea588301
arielboyu <arielboyu@gmail.com>
2021-03-16 15:12:21 -0300

    agrego modal video

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -2,14 +2,50 @@ import React, {useState,useEffect} from 'react'
 import axios from 'axios'
 import './portfolio.css'
 import Footer from '../Footer/footer'
+import { Modal } from "react-bootstrap";
+import ReactPlayer from "react-player";
 
 export default function Portfolio(){
   const [port, setPort] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
       axios.get('https://arielsalcedoportfolio.herokuapp.com/portfolio')
       .then(
           res => setPort(res.data)
       )}, [setPort])
+
+      const MoviePalyerModal = (props) => {
+  
+    
+          return (
+            <Modal
+              {...props}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+            >
+              <Modal.Header style={{ backgroundColor: "#388d7c" }} closeButton>
+                <Modal.Title
+                  id="contained-modal-title-vcenter"
+                  style={{ color: "white", fontWeight: "bolder" }}
+                >
+                  <h1>TreeBaank</h1>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body style={{backgroundColor:"#a28aae"   }}>
+                <ReactPlayer
+                  className="container-fluid"
+                  url={"https://www.youtube.com/watch?v=ufhfOgrQmwQ"}
+                  playing
+                  width="100%"
+                ></ReactPlayer>
+              </Modal.Body>
+            </Modal>
+          );
+        }
+
+
+
   return (
     <div>
     <div  className="headerport" >
@@ -26,6 +62,27 @@ export default function Portfolio(){
                   </div>
               )}
           </div>
+          <div className="row mt-2">
+              <MoviePalyerModal
+                show={isOpen}
+                onHide={() => {
+                  setIsOpen(false);
+                }}
+              ></MoviePalyerModal>
+              <div
+                className="boton-play"
+              >
+                    <h1   style={{ fontSize: 35, color: "white",}} >Ver Video</h1>
+                <div className="carousel-center">
+                  <ion-icon
+                    style={{ fontSize: 45, color: "white", cursor: "pointer" }}
+                    onClick={() => setIsOpen(true)}
+                    name="play-outline"
+                  ></ion-icon>
+                 
+                </div>
+              </div>
+            </div>
           <Footer/>
     </div>
   )
